485f19f5bc8ceef0b6f7ee51f8bd0b6df9bcb64d
Manuel Antonio Viso Troyano <maviso@gamma26.aulas.gsyc.urjc.es>
2016-11-14 17:32:53 +0100

    He realizado la peticion con exito y estoy completando los codigos de respuesta

--- client.py
+++ client.py
@@ -5,24 +5,28 @@ Programa cliente que abre un socket a un servidor
 """
 
 import socket
+import sys
 
 # Cliente UDP simple.
 
-# Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
+if len(sys.argv) != 3:
+    sys.exit("Usage: python client.py method receiver@IP:SIPport")
 
-# Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+login = sys.argv[2].split('@')[0]
+server = sys.argv[2].split('@')[1].split(':')[0]
+port = int(sys.argv[2].split(':')[-1])
+metodo = sys.argv[1]
+peticion = metodo + " sip:" + login + "@" + server + " SIP/2.0\r\n\r\n"
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-my_socket.connect((SERVER, PORT))
+my_socket.connect((server, port))
 
-print("Enviando: " + LINE)
-my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
-data = my_socket.recv(1024)
+if metodo == "INVITE" or "ACK" or "BYE":
+    print("Enviando:", peticion)  
+    my_socket.send(bytes(peticion, 'utf-8') + b'\r\n')
+    data = my_socket.recv(1024)
 
 print('Recibido -- ', data.decode('utf-8'))
 print("Terminando socket...")
--- server.py
+++ server.py
@@ -5,7 +5,7 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import socketserver
-
+import sys
 
 class EchoHandler(socketserver.DatagramRequestHandler):
     """
@@ -18,14 +18,22 @@ class EchoHandler(socketserver.DatagramRequestHandler):
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
+            lista = line.decode('utf-8').split()
             print("El cliente nos manda " + line.decode('utf-8'))
-
             # Si no hay más líneas salimos del bucle infinito
             if not line:
                 break
+            if lista[0] == "INVITE":
+                self.wfile.write(b"\r\n" + b"SIP/2.0 100 Trying" + b"\r\n" + b"SIP/2.0 180 Ring" + b"\r\n" + b"SIP/2.0 Ok" + b"\r\n")
+            elif lista[0] == "BYE":
+                self.wfile.write(b" SIP/2.0 Ok" + b"\r\n")
+            elif line[0] != "INVITE" or "ACK" or "BYE":
+                self.wfile.write(b" SIP/2.0 405 Method Not Allowed")
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
-    serv = socketserver.UDPServer(('', 6001), EchoHandler)
+    port = int(sys.argv[2])
+    ip = sys.argv[1]
+    serv = socketserver.UDPServer((ip, port), EchoHandler)
     print("Lanzando servidor UDP de eco...")
     serv.serve_forever()
