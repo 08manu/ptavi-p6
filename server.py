8ea4996fd39f2096d602c7f5f148a642c008ed9d
Manuel Antonio Viso Troyano <maviso@gamma26.aulas.gsyc.urjc.es>
2016-11-14 17:47:41 +0100

    Estoy acabando de completar los codigos de respuesta

--- server.py
+++ server.py
@@ -23,19 +23,24 @@ class EchoHandler(socketserver.DatagramRequestHandler):
             # Si no hay más líneas salimos del bucle infinito
             if not line:
                 break
-            if lista[0] == "INVITE":
-                self.wfile.write(b"\r\n" + b"SIP/2.0 100 Trying" + b"\r\n" + 
-                                 b"SIP/2.0 180 Ring" + b"\r\n" + b"SIP/2.0 Ok" +
-                                 b"\r\n")
-            elif lista[0] == "BYE":
-                self.wfile.write(b" SIP/2.0 Ok" + b"\r\n")
-            elif line[0] != "INVITE" or "ACK" or "BYE":
-                self.wfile.write(b" SIP/2.0 405 Method Not Allowed")
+
+                if lista[0] == "INVITE":
+                    self.wfile.write(b"\r\n" + b"SIP/2.0 100 Trying" + b"\r\n" + 
+                                     b"SIP/2.0 180 Ring" + b"\r\n" + b"SIP/2.0 Ok" +
+                                     b"\r\n")
+                elif lista[0] == "BYE":
+                    self.wfile.write(b" SIP/2.0 Ok" + b"\r\n")
+                elif line[0] != "INVITE" or "ACK" or "BYE":
+                    self.wfile.write(b" SIP/2.0 405 Method Not Allowed")
+
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
-    port = int(sys.argv[2])
-    ip = sys.argv[1]
-    serv = socketserver.UDPServer((ip, port), EchoHandler)
-    print("Lanzando servidor UDP de eco...")
-    serv.serve_forever()
+    if len(sys.argv) == 3:
+        port = int(sys.argv[2])
+        ip = sys.argv[1]
+        serv = socketserver.UDPServer((ip, port), EchoHandler)
+        print("Lanzando servidor UDP de eco...")
+        serv.serve_forever()
+    else:
+        sys.exit("Usage: python server.py IP port audio_file")
