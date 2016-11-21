1982648681bed139d30f2b52897f8422b501451a
08manu <manuel.viso88@gmail.com>
2016-11-21 16:10:27 +0100

    He realizado la aprte de rtp

--- client.py
+++ client.py
@@ -11,26 +11,34 @@ import sys
 
 if len(sys.argv) != 3:
     sys.exit("Usage: python client.py method receiver@IP:SIPport")
+try:
+    login = sys.argv[2].split('@')[0]
+    server = sys.argv[2].split('@')[1].split(':')[0]
+    port = int(sys.argv[2].split(':')[-1])
+    metodo = sys.argv[1]
+    peticion = metodo + " sip:" + login + "@" + server + " SIP/2.0\r\n\r\n"
 
-login = sys.argv[2].split('@')[0]
-server = sys.argv[2].split('@')[1].split(':')[0]
-port = int(sys.argv[2].split(':')[-1])
-metodo = sys.argv[1]
-peticion = metodo + " sip:" + login + "@" + server + " SIP/2.0\r\n\r\n"
+    # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+    my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    my_socket.connect((server, port))
 
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-my_socket.connect((server, port))
-
-if metodo == "INVITE" or "ACK" or "BYE":
     print("Enviando:", peticion)  
     my_socket.send(bytes(peticion, 'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
 
-print('Recibido -- ', data.decode('utf-8'))
-print("Terminando socket...")
+    print('Recibido -- ', data.decode('utf-8'))
+    list_recib = data.decode('utf-8').split()
+    if list_recib[1] == "100" and list_recib[4] == "180" and list_recib[7] == "200":
+        metodo = "ACK"
+        peticion = metodo + " sip:" + login + "@" + server + " SIP/2.0\r\n\r\n"
+        print("Enviando", peticion)
+        my_socket.send(bytes(peticion, 'utf-8') + b'\r\n')
+    print("Terminando socket...")
 
-# Cerramos todo
-my_socket.close()
-print("Fin.")
+    # Cerramos todo
+    my_socket.close()
+    print("Fin.")
+except:
+    sys.exit("Usage: python client.py method receiver@IP:SIPport")
+        
--- server.py
+++ server.py
@@ -6,6 +6,7 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 
 import socketserver
 import sys
+import os
 
 class EchoHandler(socketserver.DatagramRequestHandler):
     """
@@ -13,34 +14,37 @@ class EchoHandler(socketserver.DatagramRequestHandler):
     """
 
     def handle(self):
-        # Escribe dirección y puerto del cliente (de tupla client_address)
-        self.wfile.write(b"Hemos recibido tu peticion")
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
-            lista = line.decode('utf-8').split()
-            print("El cliente nos manda " + line.decode('utf-8'))
-            # Si no hay más líneas salimos del bucle infinito
             if not line:
                 break
-
-                if lista[0] == "INVITE":
-                    self.wfile.write(b"\r\n" + b"SIP/2.0 100 Trying" + b"\r\n" + 
-                                     b"SIP/2.0 180 Ring" + b"\r\n" + b"SIP/2.0 Ok" +
-                                     b"\r\n")
-                elif lista[0] == "BYE":
-                    self.wfile.write(b" SIP/2.0 Ok" + b"\r\n")
-                elif line[0] != "INVITE" or "ACK" or "BYE":
-                    self.wfile.write(b" SIP/2.0 405 Method Not Allowed")
-
+            print("El cliente nos manda " + line.decode('utf-8'))
+            lista = line.decode('utf-8')
+            (metodo, direccion, sip) = lista.split()
+            if metodo == "INVITE":
+                self.wfile.write(b"SIP/2.0 100 Trying" + b"\r\n" + b"\r\n"
+                                 b"SIP/2.0 180 Ring" + b"\r\n" + b"\r\n"
+                                 b"SIP/2.0 200 OK" + b"\r\n" + b"\r\n")
+            elif metodo == "ACK":
+                aEjecutar = "./mp32rtp -i " + ip + " -p 23032 < " + fichero_audio
+                print("Vamos a ejecutar", aEjecutar)
+                os.system(aEjecutar)
+            elif metodo == "BYE":
+                self.wfile.write(b"\r\n" + b"SIP/2.0 200 OK" + b"\r\n")
+            elif metodo != "INVITE" or "BYE":
+                self.wfile.write(b"\r\n" + b"SIP/2.0 405 Method Not Allowed")
+            else:
+                self.wfile.write(b"\r\n" + b"SIP/2.0 400 Bad Request")
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
-    if len(sys.argv) == 3:
+    if len(sys.argv) == 4:
         port = int(sys.argv[2])
         ip = sys.argv[1]
+        fichero_audio = sys.argv[3]
         serv = socketserver.UDPServer((ip, port), EchoHandler)
-        print("Lanzando servidor UDP de eco...")
+        print("Listening")
         serv.serve_forever()
     else:
         sys.exit("Usage: python server.py IP port audio_file")
